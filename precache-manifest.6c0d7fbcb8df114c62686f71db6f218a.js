1e150c6b4af15bde020945af9d2356275d657972
mike56k <miha.is@mail.ru>
2020-07-22 00:02:32 +0300

    Updates

---
+++ precache-manifest.6c0d7fbcb8df114c62686f71db6f218a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483e7f34c6122fe737d86c231445f3cc",
+    "url": "/todoappreact/index.html"
+  },
+  {
+    "revision": "c44a6544dcaba2c13f3f",
+    "url": "/todoappreact/static/css/main.5bbb0620.chunk.css"
+  },
+  {
+    "revision": "d5ffc89ac4f94144d165",
+    "url": "/todoappreact/static/js/2.f557b069.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todoappreact/static/js/2.f557b069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44a6544dcaba2c13f3f",
+    "url": "/todoappreact/static/js/main.f40924d9.chunk.js"
+  },
+  {
+    "revision": "c76138d385b4b1b98ed1",
+    "url": "/todoappreact/static/js/runtime-main.debf0804.js"
+  }
+]);
