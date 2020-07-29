c6f060dcd98bb1dd3d3c68322d668a28351b750a
mike56k <miha.is@mail.ru>
2020-07-29 05:38:35 +0300

    removed console log

--- src/components/Form.js
+++ src/components/Form.js
@@ -43,7 +43,6 @@ export const Form = () => {
               // ]);
               addNote(note);
               setNote("");
-              console.log(notes);
             }
           }}
         />
